16fd6d56851854766bbd41115ee1503e250b5aad
Artyom Harutyunyan <ka6rias@gmail.com>
2023-09-09 17:47:25 +0400

    validation task fix

--- validation-02.js
+++ validation-02.js
@@ -3,16 +3,10 @@ Create a function that takes an object and a set of validation rules as property
 each property according to the rules and return a new object with validated properties.
 */
 'use strict'
-function validateObj(obj, ...validationRules) {
+function validateObj(obj, validationRules) {
   const resObj = {};
   for (let key in obj) {
-    Object.defineProperty(resObj, key, { ...validationRules, value: obj[key] })
+    Object.defineProperty(resObj, key, { ...validationRules[key], value: obj[key] })
   }
   return resObj
-}
-
-let user = {
-  name: "Tyom",
-  age: 23,
-  gender: 'male'
 }
